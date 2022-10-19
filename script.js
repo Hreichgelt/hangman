0ef46685660a1c7df636be1ba39fc77b52775e52
Hans Reichgelt <hreichgelt06@gmail.com>
2022-10-19 15:30:25 -0500

    starting with js creating array of words to use

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('boobies')
+console.log('she pointed to her boobies..... oh my god she did')
+
+var words = [
+    "plasma",
+    "Water",
+    "Earth",
+    "Wind",
+    "Fire",
+    "Captain",
+    "Planet",
+    "Hugo",
+    "Carnivore",
+    "Herbivore",
+    "Alphabet",
+    "Modular",
+    "Vortex",
+    "Meditate",
+    "Dilly"
+];
+
+// need to use math.floor and math.random to pick a random word
+var word = words[Math.floor(Math.random() * words.length)];
