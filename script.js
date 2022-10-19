94f076d0782fac50628b79c28930beb1fbfb69fe
Hans Reichgelt <hreichgelt06@gmail.com>
2022-10-19 15:34:37 -0500

    math floor and random as well as empty array for answers

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log('she pointed to her boobies..... oh my god she did')
-
+// creating list of possible words
 var words = [
     "plasma",
     "Water",
@@ -20,3 +20,8 @@ var words = [
 
 // need to use math.floor and math.random to pick a random word
 var word = words[Math.floor(Math.random() * words.length)];
+
+// create empty array for user input answers
+for (var i = 0; i < word.length; i++) {
+    answerArray[i] = "_";
+}
